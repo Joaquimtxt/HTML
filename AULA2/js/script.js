c320836e4370d0e20dc0e9238e5384f6508a4bbc
Joaquim G. de Souza <jocaq124@gmail.com>
2024-03-26 10:16:37 -0300

    alterações dia 26/3

--- AULA2/js/script.js
+++ AULA2/js/script.js
@@ -1,3 +1,10 @@
 alerta = () => {
-    alert("Olá, eu sou um alerta! 🤑🤑")
-}
+  alert("Olá, eu sou um alerta! 🤑🤑");
+};
+const enviar = () => {
+  let nome = document.getElementById("nome").value;
+  
+document.getElementById("resposta").innerHTML = `<h2 class="textoCentro"> Olá ${nome}, formulário enviado com sucesso!</h2>`
+
+  nome == "" ? alert(`Bem vindo!`) : alert(`Olá ${nome}, bem vindo!`);
+};
